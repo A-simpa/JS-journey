c9a32a60db5e5be39ecd0a6c74f8de32d00a0c35
Mohammed Abdulmujeeb <mohammedabdulmujeeb16@gmail.com>
2022-05-20 01:27:24 +0100

    loop,concatenation,shadowing

--- 0x00-program_structure/0-triangle.js
+++ 0x00-program_structure/0-triangle.js
@@ -1,8 +1,14 @@
-// declare variable use to loop through
+// declare variable use to loop
+
 let hashCount = 0;
 let hash = ""
 
 for (; hashCount < 7; hashCount++){
+	// yes concatenation works in Javascript
+	// another concept used here is called Shadowing
 	hash = hash + "#"
 	console.log(hash);
 }
+
+// learnt that JavaScrpt does not support multiplication of string
+// # *  2 = NAN not "##"
